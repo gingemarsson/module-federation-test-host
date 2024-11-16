04f9086b47dd74961e2e12b25385c3dca445ee43
Gabriel Ingemarsson <gabriel@ingemarsson.nu>
2024-11-16 02:35:15 +0100

    add more styling

--- src/client/components/app.tsx
+++ src/client/components/app.tsx
@@ -3,12 +3,18 @@ import { App as ClientApp } from 'testremote/App';
 import "tailwindcss/tailwind.css";
 
 export const App = () => {
-    return <main className="flex min-h-screen flex-col items-center justify-between p-8 md:p-16">
+    return <main className="flex min-h-screen flex-col items-center p-8 md:p-16">
         <div className="max-w-7xl w-full">
             <div className="bg-white shadow-md rounded px-6 pt-4 pb-6 mb-4 w-full text-black">Hello, host!</div>
         </div>
 
         <ClientApp apiUrl='/apiproxy/client_test_remote' />
+
+        <div className="grow" />
+
+        <footer className="text-center text-gray-500 text-sm">
+            <p>Client Test Host</p>
+        </footer>
         
     </main>
 }
